c9a28fd3e33eee77e0cd356c5e6c99856fa0bc11
Soumyodeep-Das <linux.programmer.06@gmail.com>
2025-02-10 19:32:09 +0530

    smart contract updated

--- web3/contracts/Fundraise.sol
+++ web3/contracts/Fundraise.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Fundraise {
+    // Memo struct to store details of each donation
     struct Memo {
         string name;
         string message;
@@ -11,19 +12,40 @@ contract Fundraise {
         address from;
     }
 
+    // Array to store all memos
     Memo[] public memos;
-    address payable owner;
 
+    // Address of the contract owner
+    address payable public owner;
+
+    // Event to emit when a donation is received
+    event DonationReceived(address indexed from, uint256 amount, string name, string message);
+
+    // Constructor to set the owner of the contract
     constructor() {
         owner = payable(msg.sender);
     }
 
+    // Function to donate to the contract
     function donate(
         string calldata name,
         string calldata message
     ) external payable {
+        // Ensure the donation amount is greater than 0
         require(msg.value > 0, "Please pay more than 0 Sonic");
+
+        // Transfer the donation amount to the owner
         owner.transfer(msg.value);
+
+        // Add the memo to the memos array
         memos.push(Memo(name, message, block.timestamp, msg.sender));
+
+        // Emit the DonationReceived event
+        emit DonationReceived(msg.sender, msg.value, name, message);
+    }
+
+    // Function to get all memos
+    function getMemos() external view returns (Memo[] memory) {
+        return memos;
     }
 }
